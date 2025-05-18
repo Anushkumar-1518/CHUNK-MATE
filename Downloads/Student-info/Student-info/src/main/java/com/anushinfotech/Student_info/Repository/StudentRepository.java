ab3d6070858d6a6ef95118b4679c424934289f8d
Anushkumar-1518 <126445659+Anushkumar-1518@users.noreply.github.com>
2025-05-18 15:52:09 +0530

    Delete Downloads/Student-info/Student-info directory

--- Downloads/Student-info/Student-info/src/main/java/com/anushinfotech/Student_info/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anushinfotech.Student_info.Repository;
-
-import com.anushinfotech.Student_info.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
